2a84dd004045b604f03d06b1e8abaeae3eb2cd2d
oaitbenh-21 <0xoabh@gmail.com>
2025-08-22 19:03:46 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
-use std::process;
-
-
-fn main() {
-     loop {
-        // Uncomment this block to pass the first stage
-        print!("$ ");
-        io::stdout().flush().unwrap();
-
-        // Wait for user input
-        let mut input = String::new();
-        // io::stdin().read_line(&mut input).unwrap();
-        io::stdin().read_line(&mut input);
-        
-        if input.trim() == "exit" {
-            process::exit(1);
-        }
-        println!("{}: command not found", input.trim());
-     }
+#[derive(PartialEq)]
+enum Status {
+    Quotes(char),
+    Word,
+    Operation(char),
+    Null,
 }
+
+struct Code {
+    input: String,
+    tokens: Vec<String>,
+}
+
+impl Code {
+    fn new(input: &str) -> Code {
+        let mut tkns = Code {
+            input: input.to_string(),
+            tokens: Vec::new(),
+        };
+        let mut status: Status = Status::Null;
+        let chars: Vec<char> = input.chars().collect::<Vec<char>>();
+        let mut word: String = String::new();
+	let input_len = input.len();
+        let mut index: usize = 0;
+        while index < input.len() {
+            match chars[index] {
+                '"' | '\'' => {
+                    if matches!(status, Status::Quotes(c) if c == chars[index]) {
+			tkns.tokens.push(word);
+			status = Status::Null;
+			word = String::new();
+                    } else if status == Status::Null {
+			status = Status::Quotes(chars[index]);
+                    } else {
+			word.push(chars[index]);
+                    }
+                }
+		'|' | '&' | '>' | '<' | ';' => {
+		    if matches!(status, Status::Quotes(_)) || matches!(status, Status::Operation(c) if c == chars[index]) {
+			word.push(chars[index]);
+		    } else if status != Status::Null {
+			tkns.tokens.push(word);
+			word = String::new();
+		    }
+		    if status == Status::Word || status == Status::Null {
+			status = Status::Operation(chars[index]);
+		    }
+		}
+		' ' if word.len() != 0 => {
+		    tkns.tokens.push(word);
+		}
+		_=> {
+		    tkns.tokens.push(word);
+		}
+            }
+	    index += 1;
+        }
+	println!("Tokens: {:?}", tkns.tokens);
+	tkns
+    }
+}
+
+fn main() {}
