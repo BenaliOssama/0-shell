9781f5472d5a188b4596edcb2b139460dd14a38b
obenali <oussama.benali@ump.ac.ma>
2025-08-31 18:59:32 +0100

    add some changesg

--- executer/src/lib.rs
+++ executer/src/lib.rs
@@ -4,49 +4,24 @@ use std::process::Command;
 use commands::Registry;
 pub use commands::Cmd;
 
+// https://www.joshmcguigan.com/blog/build-your-own-shell-rust/?utm_source=chatgpt.com
+
 pub fn exec(cmd: Cmd) -> Result<(), Box<dyn Error>> {
     let registry = Registry::new();
 
-    // 1. Built-in
+    // Built-in
     if registry.has(&cmd) {
         registry.run(cmd);
         return Ok(());
     }
 
-    // 2. External in env_dir
+    // External in env_dir
     let path = env::var(&cmd.cmd)?;
 
     let status = Command::new(path).args(cmd.args).spawn()?.wait()?;
 
-    let entries = fs::read_dir(&dir)?;
-
-    let mut found_path = None;
-    for entry in entries {
-        let entry = entry?;
-        if let Some(filename) = entry.file_name().to_str() {
-            if filename == cmd.cmd {
-                found_path = Some(entry.path());
-                break;
-            }
-        }
-    }
-
-    if let Some(path) = found_path {
-        /*_______________________can be tested___________________________*/
-        // use Builder pattern to create a new command
-        // let output = Command::new("ls") // create a new command
-        //     .arg("-la") // add argument(s)
-        //     .output() // execute the command and capture output
-        //     .expect("Failed to execute command"); // handle any errors
-        /*_______________________________________________________________*/
-        let status = Command::new(path).args(cmd.args).spawn()?.wait()?;
-
-        if status.success() {
-            Ok(())
-        } else {
-            Err(format!("command '{}' exited with {:?}", cmd.cmd, status).into())
-        }
-    } else {
-        Err(format!("command not found: {}", cmd.cmd).into())
-    }
+    if !status.success() {
+        return Err(format!("command '{}' exited with {:?}", cmd.cmd, status).into());
+    } 
+    Ok(()) 
 }
